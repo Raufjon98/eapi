14dfea97b8f208917219f0170413733883a97ff4
raufdzhon_aliboev <raufjonaliboev77@gmail.com>
2023-09-27 23:09:41 +0900

    Product Resource created!

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,10 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-Route::apiResource('/products', 'ProductController');
 
-Route::group(['prefix'=>'products'], function(){
-    Route::apiResource('/{product}/reviews', 'ReviewController');
-});
+// Route::group(['prefix'=>'products'], function(){
+    Route::apiResources([
+        'products' => ProductController::class,
+       
+    ]);
+// });
