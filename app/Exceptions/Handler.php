b16bb887af70afc3b82fe79f996e8d7fb0980ed3
raufdzhon_aliboev <raufjonaliboev77@gmail.com>
2023-10-04 22:09:56 +0900

    Handle Exceptions!

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,12 @@
 
 namespace App\Exceptions;
 
+use Facade\FlareClient\Http\Response;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
+
 
 class Handler extends ExceptionHandler
 {
@@ -37,5 +41,17 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($request->expectsJson()) {
+            if ($exception instanceof ModelNotFoundException) {
+                return response()->json(['errors' => 'Model not found!'], 404);
+            }
+            if ($exception instanceof NotFoundHttpException) {
+                return response()->json(['errors' => 'Incorrect route!'], 404);
+            }
+        }
     }
 }
